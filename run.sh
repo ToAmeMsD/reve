e4c93be721d1c548c58014f72dcd955b2ea0e867
ToSeeAll <qwer2846579@gmail.com>
2021-12-08 22:38:36 +0800

    update cloudreve.db conf

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-cat <<-EOF > /root/cloudreve/mycloudreve.ini
-[System]
-; 运行模式
-Mode = master
-; 监听端口
-Listen = :${PORT}
-; 是否开启 Debug
-Debug = false
-SessionSecret = qygUUWWIdnhriqqSoF3bvxQ3AMb7kc5a0GAZeSXDCY3s7WBvFjVCgFqKAXmQcRUK
-HashIDSalt = S1Y1KO3CdHQwGg1nNrJ7vmOA0QWobt38wf97pG6ZPcfLDaxc8umyJC9y0bmsCkJX
-EOF
+#cat <<-EOF > /root/cloudreve/mycloudreve.ini
+#[System]
+#; 运行模式
+#Mode = master
+#; 监听端口
+#Listen = :${PORT}
+#; 是否开启 Debug
+#Debug = false
+#SessionSecret = qygUUWWIdnhriqqSoF3bvxQ3AMb7kc5a0GAZeSXDCY3s7WBvFjVCgFqKAXmQcRUK
+#HashIDSalt = S1Y1KO3CdHQwGg1nNrJ7vmOA0QWobt38wf97pG6ZPcfLDaxc8umyJC9y0bmsCkJX
+#EOF
 /root/cloudreve/cloudreve -c /root/cloudreve/mycloudreve.ini
