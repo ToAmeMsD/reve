50044de05f6589db5f64bf46f97cc500e9c5a754
ToSeeAll <qwer2846579@gmail.com>
2021-12-09 22:12:12 +0800

    add env

--- run.sh
+++ run.sh
@@ -1,15 +1,29 @@
 #!/bin/sh
-#cat <<-EOF > /root/cloudreve/mycloudreve.ini
-#[System]
-#; 运行模式
-#Mode = master
-#; 监听端口
-#Listen = :${PORT}
-#; 是否开启 Debug
-#Debug = false
-#SessionSecret = qygUUWWIdnhriqqSoF3bvxQ3AMb7kc5a0GAZeSXDCY3s7WBvFjVCgFqKAXmQcRUK
-#HashIDSalt = S1Y1KO3CdHQwGg1nNrJ7vmOA0QWobt38wf97pG6ZPcfLDaxc8umyJC9y0bmsCkJX
-#EOF
-echo ${MyEnv}
+cat <<-EOF > /root/cloudreve/mycloudreve.ini
+[System]
+; 运行模式
+Mode = master
+; 监听端口
+Listen = :${PORT}
+; 是否开启 Debug
+Debug = false
+SessionSecret = ${SESSION_SECRET}
+HashIDSalt = ${HASH_ID_SALT}
+[Database]
+; 数据库类型，目前支持 sqlite/mysql/mssql/postgres
+Type = postgres
+; MySQL 端口
+Port = ${DATABASE_PORT}
+; 用户名
+User = ${DATABASE_USER}
+; 密码
+Password = ${DATABASE_PASSWD}
+; 数据库地址
+Host = ${DATABASE_HOST}
+; 数据库名称
+Name = ${DATABASE_NAME}
+; 数据表前缀
+TablePrefix = cd_
+EOF
 /root/cloudreve/cloudreve -c /root/cloudreve/mycloudreve.ini
 #/root/cloudreve/cloudreve
